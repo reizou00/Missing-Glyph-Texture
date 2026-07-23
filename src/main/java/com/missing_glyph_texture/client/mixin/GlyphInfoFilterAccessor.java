bd8055008cb5d54f2112aa592241eaf4b90bad28
reizo00 <reizou000@gmail.com>
2026-07-23 18:54:48 +0900

    ライセンスの...ヘッダー？書くの忘れてた

--- src/main/java/com/missing_glyph_texture/client/mixin/GlyphInfoFilterAccessor.java
+++ src/main/java/com/missing_glyph_texture/client/mixin/GlyphInfoFilterAccessor.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2026 reizou00
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 package com.missing_glyph_texture.client.mixin;
 
 import com.mojang.blaze3d.font.GlyphInfo;
